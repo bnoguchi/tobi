f99ace049622dae2b739b1611196d1ee36fc9b85
bnoguchi <brian.noguchi@gmail.com>
2011-07-05 23:59:50 -0700

    Tests + code for: Cookie jar can now handle multiple domains. Moreover, cookie jar handles "; domain=.foo.com; " appropriately. ... In anticipation of upcoming true multi-domain browser support.

--- test/cookie.jar.test.js
+++ test/cookie.jar.test.js
@@ -15,7 +15,7 @@ function expires(ms) {
 module.exports = {
   'test .get() expiration': function(done){
     var jar = new Jar;
-    var cookie = new Cookie('sid=1234; path=/; expires=' + expires(1000));
+    var cookie = new Cookie('sid=1234; path=/; domain=foo.com; expires=' + expires(1000));
     jar.add(cookie);
     setTimeout(function(){
       var cookies = jar.get({ url: 'http://foo.com/foo' });
@@ -31,15 +31,15 @@ module.exports = {
   
   'test .get() path support': function(){
     var jar = new Jar;
-    var a = new Cookie('sid=1234; path=/');
-    var b = new Cookie('sid=1111; path=/foo/bar');
-    var c = new Cookie('sid=2222; path=/');
+    var a = new Cookie('sid=1234; domain=foo.com; path=/');
+    var b = new Cookie('sid=1111; domain=foo.com; path=/foo/bar');
+    var c = new Cookie('sid=2222; domain=foo.com; path=/');
     jar.add(a);
     jar.add(b);
     jar.add(c);
 
     // should remove the duplicates
-    jar.cookies.should.have.length(2);
+    jar.cookies['foo.com'].should.have.length(2);
 
     // same name, same path, latter prevails
     var cookies = jar.get({ url: 'http://foo.com/' });
@@ -52,8 +52,8 @@ module.exports = {
     cookies[0].should.equal(b);
 
     var jar = new Jar;
-    var a = new Cookie('sid=1111; path=/foo/bar');
-    var b = new Cookie('sid=1234; path=/');
+    var a = new Cookie('sid=1111; domain=foo.com; path=/foo/bar');
+    var b = new Cookie('sid=1234; domain=foo.com; path=/');
     jar.add(a);
     jar.add(b);
 
@@ -66,11 +66,11 @@ module.exports = {
     cookies[0].should.equal(b);
 
     var jar = new Jar;
-    var a = new Cookie('sid=1111; path=/foo/bar');
-    var b = new Cookie('sid=3333; path=/foo/bar');
-    var c = new Cookie('pid=3333; path=/foo/bar');
-    var d = new Cookie('sid=2222; path=/foo/');
-    var e = new Cookie('sid=1234; path=/');
+    var a = new Cookie('sid=1111; domain=foo.com; path=/foo/bar');
+    var b = new Cookie('sid=3333; domain=foo.com; path=/foo/bar');
+    var c = new Cookie('pid=3333; domain=foo.com; path=/foo/bar');
+    var d = new Cookie('sid=2222; domain=foo.com; path=/foo/');
+    var e = new Cookie('sid=1234; domain=foo.com; path=/');
     jar.add(a);
     jar.add(b);
     jar.add(c);
@@ -89,5 +89,27 @@ module.exports = {
     var cookies = jar.get({ url: 'http://foo.com/' });
     cookies.should.have.length(1);
     cookies[0].should.equal(e);
+  },
+
+  'test .get() domain support': function(){
+    var jar = new Jar;
+    var a = new Cookie('idA=1234; domain=.foo.com; path=/');
+    var b = new Cookie('idB=1111; domain=www.foo.com; path=/');
+    var c = new Cookie('idC=2222; domain=admin.foo.com; path=/');
+    jar.add(a);
+    jar.add(b);
+    jar.add(c);
+
+    // any request to *.foo.com should include the domain cookies for
+    // *.foo.com + the cookies for .foo.com
+    var cookies = jar.get({ url: 'http://www.foo.com/' });
+    cookies.should.have.length(2);
+    cookies.should.contain(a);
+    cookies.should.contain(b);
+
+    cookies = jar.get({ url: 'http://admin.foo.com/' });
+    cookies.should.have.length(2);
+    cookies.should.contain(a);
+    cookies.should.contain(c);
   }
 };
